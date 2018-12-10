edc7bda5d4cd62c319dab74aefe962cb16c77c57
elson.mathew <elson.mathew@malauzai.com>
2018-12-10 11:44:41 -0600

    Remove debugs

--- app/src/main/java/com/example/elsonmathew/flickrclone/ViewModel/FlickerFeedViewModel.java
+++ app/src/main/java/com/example/elsonmathew/flickrclone/ViewModel/FlickerFeedViewModel.java
@@ -26,31 +26,24 @@ public class FlickerFeedViewModel extends ViewModel {
 	public LiveData<List<Items>> getFeeds() {
 		flickerFeedsList = null;
 		if (flickerFeedsList == null) {
-			Log.d("78741", "feedList null");
-
 			flickerFeedsList = new MutableLiveData<List<Items>>();
 			loadFeed();
 		}
-		Log.d("78741", "returning feed");
 		return flickerFeedsList;
 	}
 
 	private void loadFeed() {
-
-		Log.d("78741", "loadFeed");
 		GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
 		Call<FlickrResponse> call = service.getFlickrResponse();
 		call.enqueue(new Callback<FlickrResponse>() {
 			@Override
 			public void onResponse(Call<FlickrResponse> call, Response<FlickrResponse> response) {
-				Log.d("78741", "onRESPONSE Received");
 				flickerFeedsList.setValue(response.body().items);
 
 			}
 
 			@Override
 			public void onFailure(Call<FlickrResponse> call, Throwable t) {
-//				swipeRefreshLayout.setRefreshing(false);
 				Log.d("78741", t.toString());
 			}
 		});
